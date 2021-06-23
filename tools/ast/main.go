510e450b0a75c47c11f826e19f60778f0de0f9d4
Eike Spang <eike@iMac.home>
2021-06-23 20:17:46 +0200

    implement tool to write token types

--- tools/ast/main.go
+++ tools/ast/main.go
@@ -1,24 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+// Cut cuts s around the first instance of sep,
+// returning the text before and after sep.
+// The found result reports whether sep appears in s.
+// If sep does not appear in s, cut returns s, "", false.
+func Cut(s, sep string) (before, after string, found bool) {
+	if i := strings.Index(s, sep); i >= 0 {
+		return s[:i], s[i+len(sep):], true
+	}
+	return s, "", false
+}
 
 func defineAST(outputDir string, asts ...string) error {
 	filename := filepath.Join(outputDir, "expressions.go")
 	_, err := os.Stat(filename)
-	var targetFile os.File
+	var targetFile *os.File
 	if err != nil {
 		if os.IsNotExist(err) {
-			// create
+			targetFile, err = os.Create(filename)
+			if err != nil {
+				return err
+			}
 		} else {
 			return err
 		}
 	} else {
-
+		filename := filepath.Join(outputDir, "expression_tmp.go")
+		targetFile, err = os.Create(filename)
+		if err != nil {
+			return err
+		}
 	}
+
+	_ = targetFile
+
+	targetFile.WriteString("package parser")
+	targetFile.WriteString("\n")
+	targetFile.WriteString("import (")
+	targetFile.WriteString("\t\"fmt\"")
+	targetFile.WriteString(")")
+	targetFile.WriteString("\n")
+
+	for _, ast := range asts {
+		b, a, ok := Cut(ast, ":")
+		if !ok {
+			return errors.New("expect everey ast to contain one colon")
+		}
+		typeName := strings.TrimSpace(b)
+		fields := strings.TrimSpace(a)
+
+		targetFile.WriteString("type " + typeName + " struct {")
+		for _, field := range strings.Split(fields, ", ") {
+			targetFile.WriteString("\t" + field)
+		}
+		targetFile.WriteString("}")
+	}
+
+	return nil
 }
 
 func main() {
